cd5fe1ac56d52baef3b12bea81f1646ebe3c9b1d
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-03-16 14:48:52 -0500

    created question model and added quiz validators

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,11 @@
 class Question < ApplicationRecord
+  validates :score, presence: true
+  validates :question_type, presence: true
+  validates :prompt, presence: true
+  validates :answer, presence: true
+  
+  
+  
+  
   belongs_to :quiz
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,2 +1,9 @@
 class Quiz < ApplicationRecord
+    :tite, presence: true
+    :category, presence: true
+    :description, presence: true
+    :created_date, presence: true
+    :total_score_possible, presence: true
+    :created_by, presence: true
+
 end
