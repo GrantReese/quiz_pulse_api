15f344488a3f21c293c1ddd6ee2356f0520babac
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-04-05 17:41:56 -0500

    testing routing for do_quiz

--- app/controllers/do_quiz_controller.rb
+++ app/controllers/do_quiz_controller.rb
@@ -1,18 +1,20 @@
 class DoQuizController < ApplicationController
-  before_action :set_do_quiz, only: [ :show, :update, :destroy ]
-  before_action :authenticate_request, except: [:index]
+  before_action :set_do_quiz, only: [ :show, :destroy ]
+  before_action :authenticate_request, except: [:index, :show, :create, :destroy]
 
   def index
     do_quizzes = DoQuiz.all
+
     render json: do_quizzes, status: 200
   end
 
   def show
-    do_quiz = DoQuiz.find(params[:id])
-    render json: do_quiz, status: 200
+    
+    render json: @do_quiz, status: 200
   end
 
   def create
+    # do_quiz = @current_user.do_quizzes.create(do_quiz_params)
     do_quiz = DoQuiz.create(do_quiz_params)
     if do_quiz.save
       render json: do_quiz, status: :created
@@ -22,14 +24,20 @@ class DoQuizController < ApplicationController
   end
 
   def destroy
-    do_quiz = DoQuiz.find(params[:id])
-    if do_quiz.destroy
+    
+    if @do_quiz.destroy
       render json: nil, status: :ok
     else
       render json: do_quiz.errors, status: :unprocessable_entity
     end
   end
   
+  private 
+
+  def set_do_quiz
+    do_quiz = DoQuiz.find(params[:id])
+  end
+
   def do_quiz_params
     params.permit(:user_id, :quiz_id, :completed_status)
   end
