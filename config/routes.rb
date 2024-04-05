a08b2de9dd6277934417c84226ed9b778f1dcad0
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-04-04 20:33:55 -0500

    Update routes.rb
    updated routes- do quiz not working

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :quizzes
   resources :questions
+  resources :do_quiz
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
