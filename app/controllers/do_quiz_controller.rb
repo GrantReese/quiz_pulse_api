3c868ded57e614eb156a6b7edbf96bcfd933f6a3
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-04-04 20:19:30 -0500

    Update do_quiz_controller.rb

--- app/controllers/do_quiz_controller.rb
+++ app/controllers/do_quiz_controller.rb
@@ -1,7 +1,34 @@
 class DoQuizController < ApplicationController
-  def create
+  before_action :set_do_quiz, only: %i[ show update destroy ]
+  
+  def index
+    do_quizzes = DoQuiz.all
+    render json: do_quizzes, status: 200
   end
 
+  def show
+    do_quiz = DoQuiz.find(params[:id])
+    render json: do_quiz, status: 200
+  end
+
+  def create
+    do_quiz = DoQuiz.create(do_quiz_params)
+    if do_quiz.save
+      render json: do_quiz, status: :created
+    else
+      render json: do_quiz.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    do_quiz = DoQuiz.find(params[:id])
+    if do_quiz.destroy
+      render json: nil, status: :ok
+    else
+      render json: do_quiz.errors, status: :unprocessable_entity
+    end
+  end
+  
   def do_quiz_params
     params.permit(:user_id, :quiz_id, :completed_status)
   end
