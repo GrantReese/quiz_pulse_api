c1935feb6836c03295b986ec6efc8f355ef21e3f
Grant Reese <119977613+GrantReese@users.noreply.github.com>
2024-04-18 13:53:18 -0500

    Update 20240322003509_remove_completed_status_and_boolean_from_do_quiz.rb

--- db/migrate/20240322003509_remove_completed_status_and_boolean_from_do_quiz.rb
+++ db/migrate/20240322003509_remove_completed_status_and_boolean_from_do_quiz.rb
@@ -1,5 +1,5 @@
-# class RemoveCompletedStatusAndBooleanFromDoQuiz < ActiveRecord::Migration[7.1]
-#   def change
-#     remove_column :do_quizzes, :boolean
-#   end
-# end
+class RemoveCompletedStatusAndBooleanFromDoQuiz < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :do_quizzes, :boolean
+  end
+end
